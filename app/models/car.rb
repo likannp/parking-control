f9102c8d59b9335ef7ff61ead550fb07055609b9
Elisangela <elisangela00silva@gmail.com>
2022-06-04 18:56:24 -0300

    car model

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,11 @@
 class Car < ApplicationRecord
   has_many :parking_histories
+  validate :invalid_plate_format
+
+  private
+  
+  def invalid_plate_format
+    validate = plate.match(/[a-zA-Z]{3}-[0-9]{4}/)
+    errors.add(:plate, :invalid, message: "Invalid Car Plate") if validate.nil?
+  end
 end
