d80fcec69f7650f03f0e7d9588ca914f62c9bcd3
Elisangela <elisangela00silva@gmail.com>
2022-06-05 12:58:05 -0300

    create action out

--- app/controllers/parking_histories_controller.rb
+++ app/controllers/parking_histories_controller.rb
@@ -14,15 +14,28 @@ class ParkingHistoriesController < ApplicationController
   end
 
   def out
+    #erro em caso de carro não cadastrado
+    #erro em caso de pagamento
+    # A busca leva em consideração o ultimo registro realizado sem saída.
+    parking_history = ParkingHistory.joins(:car).where(car: {plate: params[:plate].downcase}, out_at: nil ).last
+
+    return render json: {errors: "Parking record not found."}, status: 404 if parking_history.nil?
+
+    return render json: {errors: "Parking must be paid."}, status: 422 unless parking_history.paid
+
+    parking_history.update(out_at: Time.now)
+    render status: 204
 
   end
 
   def pay
-
+    #erro em caso de carro não cadastrado
+    #sucesso em caso de carro já pago, porém adicionar teste no cenario onde ele não pode dá erro.Tem que retorna o mesmo do contexto de sucesso.
     binding.pry
   end
 
   def show
+    #erro em caso de carro não cadastrado
     binding.pry
   end
 
