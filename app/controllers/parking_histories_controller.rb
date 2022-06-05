c835b5de34ba139ce0acac62aa635680ee5f3af9
Elisangela <elisangela00silva@gmail.com>
2022-06-05 11:36:54 -0300

    ParkingHistories

--- app/controllers/parking_histories_controller.rb
+++ app/controllers/parking_histories_controller.rb
@@ -1,23 +1,24 @@
 class ParkingHistoriesController < ApplicationController
 
   def create
+
     car = Car.find_by_plate(params[:plate])
     if car.nil?
       car = Car.new(plate: params[:plate])
       return render json: {errors: car.errors.full_messages.join(', ')}, status: 400 unless car.save
     end
 
-    parking_history = ParkingHistory.new(car: car, entry_at: Time.now)
-    if parking_history.save
-      render json: {reservation: parking_history.id}
-    end
+    parking_history = ParkingHistory.create(car: car, entry_at: Time.now)
+    render json: {reservation: parking_history.id}
+ 
   end
 
   def out
-    binding.pry
+
   end
 
   def pay
+
     binding.pry
   end
 
