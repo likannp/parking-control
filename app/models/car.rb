431fcc4b15d02cc14ee4ddd35750923702642efa
Elisangela <elisangela00silva@gmail.com>
2022-06-05 12:57:40 -0300

    update car model

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,16 @@
 class Car < ApplicationRecord
   has_many :parking_histories
   validate :invalid_plate_format
+  before_save :downcase_plate!
 
   private
   
   def invalid_plate_format
     validate = plate.match(/^[a-zA-Z]{3}-[0-9]{4}$/)
     errors.add(:plate, :invalid, message: "Is Invalid.") if validate.nil?
+  end
+
+  def downcase_plate!
+    self.plate = self.plate.downcase
   end
 end
